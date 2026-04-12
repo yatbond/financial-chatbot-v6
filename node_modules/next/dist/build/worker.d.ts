9ae13524c99c079689640d62172fc38bbab0eafa
yatbond <derrickpang@gmail.com>
2026-04-12 17:13:52 +0800

    Add .gitignore, remove node_modules

--- node_modules/next/dist/build/worker.d.ts
+++
@@ -1,4 +0,0 @@
-import '../server/require-hook';
-import '../server/lib/cpu-profile';
-export { getDefinedNamedExports, hasCustomGetInitialProps, isPageStatic, } from './utils';
-export { exportPages } from '../export/worker';
--- node_modules/next/error.d.ts
+++
@@ -1,7 +0,0 @@
-// Pages Router only
-import Error from './dist/api/error'
-export * from './dist/api/error'
-export default Error
-
-export { unstable_catchError } from './dist/api/error'
-export type { ErrorInfo } from './dist/api/error'
